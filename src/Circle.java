671455dd8eb9258713c5859bf50f68f49fdc1dca
ItachiCodes <52618896+ItachiCodes@users.noreply.github.com>
2021-11-11 00:56:52 -0800

    Initial commit

---
+++ src/Circle.java
@@ -0,0 +1,64 @@
+public class Circle
+{
+    private double radius;
+    private double  area;
+    private final double pi = Math.PI;
+
+    public void setRadius(double userRadius){
+        this.radius = userRadius;
+    }
+
+    public void setArea(double userArea){
+        this.area = userArea;
+    }
+
+
+    //Method to find the Area
+    public double getArea(){
+        //--> formula : (pi) (r) (r)
+        double area = pi * radius * radius;
+
+        return area;
+    }
+
+    //Method/Function to find Circumference
+    public double getCircumference(){
+        //--> formula : (2) (pi) (r)
+        double circumference = 2 * pi * radius;
+
+        return circumference;
+    }
+
+    //Method/Function to Diameter
+    public double getDiameter(){
+        //--> formula : d= (2) (r)
+        double diameter = 2 * radius;
+
+        return diameter;
+    }
+
+    //Method/Function for Radius
+    public double getRadius(){
+        //--> formula : ( sqrt( A/pi) )
+        double radius = Math.sqrt( (area / pi ) );
+
+       // double Area = this
+
+        return radius;
+    }
+
+
+    //print
+    public void printCircleInfo()
+    {
+        System.out.println();
+        System.out.println("__________________________");
+        System.out.printf("Circle's Area: %.2f\n", getArea());
+        System.out.printf("Circle's Circumference: %.2f\n", getCircumference());
+        System.out.printf("Circle's Diameter: %.2f\n", getDiameter());
+        System.out.printf("Circle's Radius: %.2f\n", getRadius());
+        System.out.println("__________________________");
+        System.out.println();
+    }
+
+}
---
+++ src/DriverShapesClass.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class DriverShapesClass
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        Circle circleObj = new Circle();
+
+        System.out.print("Enter Circle's Radius: ");
+        circleObj.setRadius(sc.nextDouble());
+
+        System.out.print("Enter Circle's Area: ");
+        circleObj.setArea(sc.nextDouble());
+
+        circleObj.printCircleInfo();
+
+    }
+
+}
